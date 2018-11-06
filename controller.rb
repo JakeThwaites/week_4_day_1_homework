e2e8ef2b2a587a0b6d7614b505c0b9047c6cca65
Jake Thwaites <jakethwaites@hotmail.co.uk>
2018-11-06 17:12:51 +0000

    added single player option

--- controller.rb
+++ controller.rb
@@ -3,12 +3,18 @@ require ('sinatra/contrib/all') if development?
 require_relative('models/rps')
 also_reload('./models/*')
 
-get '/:choice1/:choice2' do
+get '/multiplayer/:choice1/:choice2' do
   first_choice = params[:choice1].to_s
   second_choice = params[:choice2].to_s
 
-  @calcuation = RockPaperScissors.check_result(first_choice, second_choice)
+  @calculation = RockPaperScissors.check_result__multiplayer(first_choice, second_choice)
   erb(:result)
+end
+
+get '/single-player/:choice' do
+  player_choice = params[:choice].to_s
+  @singeplayer_calc = RockPaperScissors.check_result__single_player(player_choice)
+  erb(:singleplayer_result)
 end
 
 get '/' do
--- models/rps.rb
+++ models/rps.rb
@@ -5,21 +5,38 @@ class RockPaperScissors
   end
 
 
-  def self.check_result(choice1, choice2)
+  def self.check_result__single_player(choice)
+
+    options = ["rock", "paper", "scissors"]
+    computer_choice = options.sample
+
+    winning = {"rock" => "scissors", "scissors" => "paper", "paper" => "rock"}
+
+    if choice == computer_choice
+      return "It's a draw"
+    elsif winning[choice] == computer_choice
+      return "Player 1 wins by playing #{choice}!"
+    elsif winning[computer_choice] == choice
+      return "Computer wins by playing #{computer_choice}!"
+    else
+      return "These options aren't valid!"
+    end
+  end
+
+  def self.check_result__multiplayer(choice1, choice2
+)
 
     winning = {"rock" => "scissors", "scissors" => "paper", "paper" => "rock"}
 
     if choice1 == choice2
       return "It's a draw"
     elsif winning[choice1] == choice2
-      return "Player 1 plays #{choice1}. Player 1 wins!"
+      return "Player 1 wins by playing #{choice1}!"
     elsif winning[choice2] == choice1
-      return "Player 2 plays #{choice2}. Player 2 wins!"
+      return "Player 2 wins by playing #{choice2}!"
     else
       return "These options aren't valid!"
     end
   end
 
 end
-
-game = RockPaperScissors.check_result("paper", "rock")
